f62531a4a24870a0cac54ccce7f5c72f3517089e
mlarotonda <mauricio.larotonda@fravega.com.ar>
2023-10-28 18:05:03 -0300

    Screens navegacion

---
+++ screens/MiColeccion/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default ({ navigation }) => {
+  return (
+    <View>
+      <Text> </Text>
+      <Text> </Text>
+      <Text>Mi coleccion</Text>
+    </View>
+  )
+}
