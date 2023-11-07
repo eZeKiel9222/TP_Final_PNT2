2123f3b82a9601970c65102de94528f248f9b9b8
mlarotonda <mauricio.larotonda@fravega.com.ar>
2023-11-06 23:08:37 -0300

    Componente header

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,19 +1,45 @@
-import { useContext } from 'react';
+import { useContext, useState, useCallback } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 import { View, Text, Button, SafeAreaView } from 'react-native';
 import GlobalContext from '../../services/GlobalContext';
+import Header from '../../components/Header/index.js';
+import styles from '../../styles/styles.js';
+import MazosService from "../../services/mazos";
 
 export default ({ navigation }) => {
 
   const { changeHome } = useContext(GlobalContext)
+  const [mazos, setMazos] = useState(null)
+
+  useFocusEffect(
+    useCallback(() => {
+      MazosService.getMazos()
+        .then(data => {
+          setMazos(data)
+        })
+    }, [])
+  );
 
   return (
     <SafeAreaView>
-      <Text> </Text>
-      <Text> </Text>
+      <Header name='Home' />
+      <Text>  </Text>
       <View>
-        <Text>Home mis mazos</Text>
-        <Button title="Ir a login" onPress={changeHome} />
+        <Button styles={styles.containerbuttons} title="Ir a login" onPress={changeHome} />
       </View>
+      <Text>  </Text>
+      <Text>  </Text>
+      {mazos ? (
+        mazos.map((mazo, index) => (
+        <View>
+          {/* <Text>Mazo: {index} - {mazo.nombreMazo}</Text> */}
+          <Text>Mazo: {index} - {mazo.fullName}</Text>
+        </View>
+      ))) : (
+        <Text>No se encontraron datos.</Text>
+      )
+      }
+
     </SafeAreaView>
   )
 }
