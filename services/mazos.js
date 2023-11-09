f37add5fbf7eee61e1aa53cbe6ffb9db6db77cb1
mlarotonda <mauricio.larotonda@fravega.com.ar>
2023-11-09 20:31:01 -0300

    Uso de variables de entorno

--- services/mazos.js
+++ services/mazos.js
@@ -1,18 +1,7 @@
 //const BASE_URL = "https://us-central1-api-nt2-ejemplo.cloudfunctions.net/app/api/read"
-const BASE_URL = "http://172.20.102.248:8080/api/mazo"
 
 const getMazos = () => {
-  return fetch(`${BASE_URL}`)
-    .then(res => {
-      if (res.status === 200) {
-        return res.json();
-      } else {
-        throw new Error("Error: Not found");
-      }
-    });
-};
-const getMazosByIdUser = (id) => {
-  return fetch(`${BASE_URL}/user/${id}`)
+  return fetch(`${process.env.URL}/api/mazo`)
     .then(res => {
       if (res.status === 200) {
         return res.json();
@@ -22,8 +11,18 @@ const getMazosByIdUser = (id) => {
     });
 };
 
+const getMazosByIdUser = (id) => {
+  return fetch(`${process.env.URL}/api/mazo/user/${id}`)
+    .then(res => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        throw new Error("Error: Not found");
+      }
+    });
+};
 
 export default {
-    getMazos,
-    getMazosByIdUser
+  getMazos,
+  getMazosByIdUser
 };
--- services/users.js
+++ services/users.js
@@ -1,31 +1,26 @@
-const users = [
-  { id: 0, username: 'Alice', password: 'password123' },
-  { id: 1, username: 'Bob', password: 'secret456' },
-  { id: 2, username: 'Charlie', password: 'secure789' },
-  { id: 3, username: 'admin', password: 'admin' }
-];
 const Login = (userLogin, userPassword) => {
   const requestBody = {
     userLogin: userLogin,
     userPassword: userPassword
   };
 
-  return fetch(`http://172.20.102.248:8080/api/user/login`, {
+  return fetch(`${process.env.URL}/api/user/login`, {
     method: "POST",
     headers: {
       'Content-Type': 'application/json' // Specify the content type as JSON
     },
     body: JSON.stringify(requestBody) // Convert the object to a JSON string
   })
-  .then(res => {
-    if (res.status === 200) {
-      return res.json();
-    } else {
-      return res.json();
-    }
-  });
+    .then(res => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        return res.json();
+      }
+    });
 }
-const Register = (Login,Password,nickName,email) => {
+
+const Register = (Login, Password, nickName, email) => {
   const requestBody = {
     userLogin: Login,
     userPassword: Password,
@@ -33,21 +28,22 @@ const Register = (Login,Password,nickName,email) => {
     email: email
   };
 
-  return fetch(`http://172.20.102.248:8080/api/user`, {
+  return fetch(`${process.env.URL}/api/user`, {
     method: "POST",
     headers: {
       'Content-Type': 'application/json' // Specify the content type as JSON
     },
     body: JSON.stringify(requestBody) // Convert the object to a JSON string
   })
-  .then(res => {
-    console.log()
-    if (res.status === 200) {
-      return res.json()
-    } else {
-      return res.json()
-    }
-})}
+    .then(res => {
+      console.log()
+      if (res.status === 200) {
+        return res.json()
+      } else {
+        return res.json()
+      }
+    })
+}
 
 export default {
   Login,
