cfb845a47b10d55ad1e69e6d9b7b858dba1a8f0a
mlarotonda <mauricio.larotonda@fravega.com.ar>
2023-11-16 12:32:46 -0300

    format code

--- screens/BuscarMazo/buscarMazo.js
+++ screens/BuscarMazo/buscarMazo.js
@@ -1,6 +1,5 @@
 import Header from '../../components/Header/index.js';
-import { useContext, useState } from 'react';
-import GlobalContext from '../../services/GlobalContext';
+import { useState } from 'react';
 import header from '../../styles/header.js'
 import { SearchBar } from '@rneui/themed';
 import { SelectList } from 'react-native-dropdown-select-list'
@@ -14,11 +13,11 @@ import MazoFlatlist from '../../components/MazoFlatlist/index.js';
 
 export default ({ navigation }) => {
 
-  const [filter,setFilter] = useState()
-  const [search,setSearch] = useState()
-  const [data,setData] = useState()
+  const [filter, setFilter] = useState()
+  const [search, setSearch] = useState()
+  const [data, setData] = useState()
 
-  const SearchData = [{key:'modo' , value:'Modo de Juego'},{key:'user' ,value:'Nickname'},{key:'name' ,value:'Nombre de Mazo'}]
+  const SearchData = [{ key: 'modo', value: 'Modo de Juego' }, { key: 'user', value: 'Nickname' }, { key: 'name', value: 'Nombre de Mazo' }]
 
   const updateSearch = (search) => {
     setSearch(search);
@@ -28,59 +27,59 @@ export default ({ navigation }) => {
   }
 
   const handleBusqueda = () => {
-        searchService.search(filter,search).then(data => {
-            console.log(data.message)
-            const midpoint = Math.ceil(data.message.length / 2);
-          console.log(midpoint)
-          const firstHalf = data.message.slice(0, midpoint);
-          console.log(firstHalf)
-          const secondHalf = data.message.slice(-midpoint);
-          console.log(secondHalf)
-          const pairedMazos = Array.from({ length: Math.min(firstHalf.length, secondHalf.length) }, (_, index) => [
-            firstHalf[index],
-            secondHalf[index]
-          ]);
+    searchService.search(filter, search).then(data => {
+      console.log(data.message)
+      const midpoint = Math.ceil(data.message.length / 2);
+      console.log(midpoint)
+      const firstHalf = data.message.slice(0, midpoint);
+      console.log(firstHalf)
+      const secondHalf = data.message.slice(-midpoint);
+      console.log(secondHalf)
+      const pairedMazos = Array.from({ length: Math.min(firstHalf.length, secondHalf.length) }, (_, index) => [
+        firstHalf[index],
+        secondHalf[index]
+      ]);
 
-          console.log(pairedMazos)
-          setData(pairedMazos);
-        }).catch(error => {
-            console.error('Error fetching mazos:', error);
-          });
+      console.log(pairedMazos)
+      setData(pairedMazos);
+    }).catch(error => {
+      console.error('Error fetching mazos:', error);
+    });
   }
 
   return (
     <View>
-    <Header name='Buscar mazo' styleHeader={header.title} styleDivider={header.divider} />
-    <Text></Text>
-    <View style={{}} >
-      <SearchBar
-        placeholder="Type Here..."
-        onChangeText={updateSearch}
-        value={search}
-        onClear={clearSearch}/>
-    <View style={{width: 200, flexDirection:'row' }}>
-    <View>
-    <Text style={homeStyles.title}>Filtro:</Text>
-    </View>
-    <View style={{marginTop:15 ,marginLeft:-10}}>
-    <SelectList
-        setSelected={(val) => setFilter(val)}
-        data={SearchData}
-        save="key"
-      />
+      <Header name='Buscar mazo' styleHeader={header.title} styleDivider={header.divider} />
+      <Text></Text>
+      <View style={{}} >
+        <SearchBar
+          placeholder="Type Here..."
+          onChangeText={updateSearch}
+          value={search}
+          onClear={clearSearch} />
+        <View style={{ width: 200, flexDirection: 'row' }}>
+          <View>
+            <Text style={homeStyles.title}>Filtro:</Text>
+          </View>
+          <View style={{ marginTop: 15, marginLeft: -10 }}>
+            <SelectList
+              setSelected={(val) => setFilter(val)}
+              data={SearchData}
+              save="key"
+            />
+          </View>
+          <View style={{ marginTop: 15, marginLeft: 15 }}>
+            <Button title='Buscar' buttonStyle={buttonStyle.blackButton}
+              titleStyle={{ fontWeight: 'bold' }}
+              onPress={handleBusqueda} />
+          </View>
+        </View>
       </View>
-      <View style={{marginTop:15, marginLeft: 15}}>
-    <Button title='Buscar' buttonStyle={buttonStyle.blackButton}
-          titleStyle={{ fontWeight: 'bold' }} 
-          onPress={handleBusqueda}/>
-    </View>
-      </View>
-    </View>
-    {data ? 
-    <MazoFlatlist mazos={data} navigation={navigation} ruta={'Detalle Mazo Buscado'}/>
+      {data ?
+        <MazoFlatlist mazos={data} navigation={navigation} ruta={'Detalle Mazo Buscado'} />
         :
         <View></View>
-    }
+      }
 
     </View>
 
--- screens/BuscarMazo/detalleMazo.js
+++ screens/BuscarMazo/detalleMazo.js
@@ -1,42 +1,40 @@
 import { useState, useCallback } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
-import { View, Text, Switch } from 'react-native';
+import { View } from 'react-native';
 import MazosService from '../../services/mazos.js'
 import CartaFlatList from '../../components/CartaFlatList/index.js';
-import { Button } from '@rneui/themed';
-import buttonStyle from '../../styles/buttons.js';
 
 export default ({ navigation, route }) => {
-    const [mazo, setMazo] = useState()
-    const [isEnabled, setIsEnabled] = useState(false);
-  
-    useFocusEffect(
-      useCallback(() => {
-        MazosService.getMazo(route.params.mazoId)
-          .then(data => {
-            console.log(data.message)
-            const midpoint = Math.ceil(data.message[0].Carta.length / 2);
-            const firstHalf = data.message[0].Carta.slice(0, midpoint);
-            const secondHalf = data.message[0].Carta.slice(-midpoint);
-  
-            const pairedCartas = Array.from({ length: Math.min(firstHalf.length, secondHalf.length) }, (_, index) => [
-              firstHalf[index],
-              secondHalf[index]
-            ]);
-  
-            setMazo(pairedCartas);
-          })
-          .catch(error => {
-            console.error('Error fetching mazos:', error);
-          });
-      }, [])
-  
-    );
+  const [mazo, setMazo] = useState()
+  const [isEnabled, setIsEnabled] = useState(false);
 
-    return (
-      <View >        
-        <CartaFlatList cartas={mazo} navigation={navigation} ruta={'Detalle Carta Buscado'} />
-      </View>
-  
-    )
+  useFocusEffect(
+    useCallback(() => {
+      MazosService.getMazo(route.params.mazoId)
+        .then(data => {
+          console.log(data.message)
+          const midpoint = Math.ceil(data.message[0].Carta.length / 2);
+          const firstHalf = data.message[0].Carta.slice(0, midpoint);
+          const secondHalf = data.message[0].Carta.slice(-midpoint);
+
+          const pairedCartas = Array.from({ length: Math.min(firstHalf.length, secondHalf.length) }, (_, index) => [
+            firstHalf[index],
+            secondHalf[index]
+          ]);
+
+          setMazo(pairedCartas);
+        })
+        .catch(error => {
+          console.error('Error fetching mazos:', error);
+        });
+    }, [])
+
+  );
+
+  return (
+    <View >
+      <CartaFlatList cartas={mazo} navigation={navigation} ruta={'Detalle Carta Buscado'} />
+    </View>
+
+  )
 }
