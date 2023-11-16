4652c1bc58cbae26e97180e0d7e6f4a057805be4
mlarotonda <mauricio.larotonda@fravega.com.ar>
2023-11-16 12:32:19 -0300

    Vista buscar carta

--- screens/BuscarMazo/index.js
+++ screens/BuscarMazo/index.js
@@ -1,7 +1,5 @@
-import Header from '../../components/Header/index.js';
 import { useContext } from 'react';
 import GlobalContext from '../../services/GlobalContext';
-import header from '../../styles/header.js'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import buscarMazo from './buscarMazo.js';
 import detalleMazo from './detalleMazo.js';
@@ -9,27 +7,25 @@ import detalleCarta from './detalleCarta.js';
 
 
 export default ({ navigation }) => {
-  const StackNavigator = createNativeStackNavigator()
+      const StackNavigator = createNativeStackNavigator()
 
-  const { changeHome } = useContext(GlobalContext)
+      const { changeHome } = useContext(GlobalContext)
 
-  return (
-    <StackNavigator.Navigator>
-    <StackNavigator.Screen name='Buscar Mazos' component={buscarMazo} options={
-          {
-                headerBackVisible: false,
-                headerShown: false
-          }} />
-    <StackNavigator.Screen name='Detalle Mazo Buscado' component={detalleMazo} options={
-          {
+      return (
+            <StackNavigator.Navigator>
+                  <StackNavigator.Screen name='Buscar Mazos' component={buscarMazo} options={
+                        {
+                              headerBackVisible: false,
+                              headerShown: false
+                        }} />
+                  <StackNavigator.Screen name='Detalle Mazo Buscado' component={detalleMazo} options={
+                        {
 
-          }} />
-    <StackNavigator.Screen name='Detalle Carta Buscado' component={detalleCarta} options={
-          {
+                        }} />
+                  <StackNavigator.Screen name='Detalle Carta Buscado' component={detalleCarta} options={
+                        {
 
-          }} />
-
-
-</StackNavigator.Navigator>
-)
+                        }} />
+            </StackNavigator.Navigator>
+      )
 }
